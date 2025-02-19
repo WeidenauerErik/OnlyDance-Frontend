e373130415e79a59a1a8b9d32f0789bbdae06c1a
Erik Weidenauer <114598675+WeidenauerErik@users.noreply.github.com>
2025-02-19 22:24:51 +0100

    Updating Prototype
    Adding the woman feature

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,265 @@
 <template>
-  <div id='morphDiv'>
-
-    <div id='leftFoot' class='foot'>
-      <div id='leftFootToes' class='toe'></div>
-      <div class='innerFootSpacer'></div>
-      <div id='leftFootHeel' class='heel'></div>
+  <div id="morphDiv">
+    <div id="manLeftFoot" class="foot">
+      <div id="manLeftFootToes" class="menToe"></div>
+      <div class="innerFootSpacer"></div>
+      <div id="manLeftFootHeel" class="menHeel"></div>
     </div>
 
-    <div id='rightFoot' class='foot'>
-      <div id='rightFootToes' class='toe'></div>
-      <div class='innerFootSpacer'></div>
-      <div id='rightFootHeel' class='heel'></div>
+    <div id="manRightFoot" class="foot">
+      <div id="manRightFootToes" class="menToe"></div>
+      <div class="innerFootSpacer"></div>
+      <div id="manRightFootHeel" class="menHeel"></div>
     </div>
 
+    <div id="womanLeftFoot" class="foot">
+      <div id="womanLeftFootToes" class="womanToe"></div>
+      <div class="innerFootSpacer"></div>
+      <div id="womanLeftFootHeel" class="womanHeel"></div>
+    </div>
+
+    <div id="womanRightFoot" class="foot">
+      <div id="womanRightFootToes" class="womanToe"></div>
+      <div class="innerFootSpacer"></div>
+      <div id="womanRightFootHeel" class="womanHeel"></div>
+    </div>
   </div>
 
-  <div id='controls'>
-    <button id='backButton' @click='BackBtn' :disabled='BackBtnDisabled'>Back</button>
-    <div id='controlsSpacer'></div>
-    <button id='nextButton' @click='NextBtn' :disabled='NextBtnDisabled'>Next</button>
+  <div id="controls">
+    <button id="backButton" @click="BackBtn" :disabled="BackBtnDisabled">Back</button>
+    <div id="controlsSpacer"></div>
+    <button id="nextButton" @click="NextBtn" :disabled="NextBtnDisabled">Next</button>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed, onMounted } from 'vue'
 
 const stepCounter = ref(0);
 const footHeightDifferenz = 55;
 const footWidthDifferenz = 25;
-let leftFoot;
-let rightFoot;
-
-let leftFootToes;
-let leftFootHeel;
-let rightFootToes;
-let rightFootHeel;
+let manLeftFoot;
+let manLeftFootHeel;
+let manLeftFootToes;
+let manRightFoot;
+let manRightFootToes;
+let manRightFootHeel;
+let womanLeftFoot;
+let womanLeftFootHeel;
+let womanLeftFootToes;
+let womanRightFoot;
+let womanRightFootToes;
+let womanRightFootHeel;
 
 let steps = [
-  { leftHeight: 0.5, leftWidth: 0.45, leftRotate: 90, leftFootToesActive: true, leftFootHeelActive:true, rightHeight: 0.2, rightWidth: 0.55, rightRotate: 90, rightFootToesActive: false, rightFootHeelActive:true },
-  { leftHeight: 0.2, leftWidth: 0.55, leftRotate: -150, leftFootToesActive: false, leftFootHeelActive:true, rightHeight: 0.25, rightWidth: 0.65, rightRotate: 150, rightFootToesActive: true, rightFootHeelActive:true },
-  { leftHeight: 0.2, leftWidth: 0.55, leftRotate: -150, leftFootToesActive: false, leftFootHeelActive:false, rightHeight: 0.25, rightWidth: 0.65, rightRotate: 150, rightFootToesActive: true, rightFootHeelActive:true },
-  { leftHeight: 0.3, leftWidth: 0.35, leftRotate: 50, leftFootToesActive: true, leftFootHeelActive:false, rightHeight: 0.3, rightWidth: 0.45, rightRotate: 190, rightFootToesActive: false, rightFootHeelActive:false }
-];
+  {
+    woman: {
+      leftFoot: {
+        height: 0.1,
+        width: 0.6,
+        rotate: 180,
+        footToesActive: true,
+        footHeelActive: false,
+      },
+      rightFoot: {
+        height: 0.1,
+        width: 0.45,
+        rotate: 180,
+        footToesActive: false,
+        footHeelActive: false,
+      },
+    },
+    man: {
+      leftFoot: {
+        height: 0.8,
+        width: 0.45,
+        rotate: 0,
+        footToesActive: true,
+        footHeelActive: false,
+      },
+      rightFoot: {
+        height: 0.8,
+        width: 0.6,
+        rotate: 0,
+        footToesActive: false,
+        footHeelActive: false,
+      },
+    },
+  },
+  {
+    woman: {
+      leftFoot: {
+        height: 0.2,
+        width: 0.6,
+        rotate: 180,
+        footToesActive: true,
+        footHeelActive: false,
+      },
+      rightFoot: {
+        height: 0.1,
+        width: 0.45,
+        rotate: 180,
+        footToesActive: false,
+        footHeelActive: false,
+      },
+    },
+    man: {
+      leftFoot: {
+        height: 0.8,
+        width: 0.45,
+        rotate: 0,
+        footToesActive: true,
+        footHeelActive: false,
+      },
+      rightFoot: {
+        height: 0.9,
+        width: 0.6,
+        rotate: 0,
+        footToesActive: false,
+        footHeelActive: false,
+      },
+    },
+  },
+  {
+    woman: {
+      leftFoot: {
+        height: 0.2,
+        width: 0.6,
+        rotate: 180,
+        footToesActive: true,
+        footHeelActive: false,
+      },
+      rightFoot: {
+        height: 0.2,
+        width: 0.45,
+        rotate: 270,
+        footToesActive: false,
+        footHeelActive: false,
+      },
+    },
+    man: {
+      leftFoot: {
+        height: 0.9,
+        width: 0.45,
+        rotate: -90,
+        footToesActive: true,
+        footHeelActive: false,
+      },
+      rightFoot: {
+        height: 0.9,
+        width: 0.6,
+        rotate: 0,
+        footToesActive: false,
+        footHeelActive: false,
+      },
+    },
+  },
+]
 
 onMounted(() => {
-  leftFoot = document.getElementById('leftFoot');
-  leftFootToes = document.getElementById('leftFootToes');
-  leftFootHeel = document.getElementById('leftFootHeel');
-  rightFoot = document.getElementById('rightFoot');
-  rightFootToes = document.getElementById('rightFootToes');
-  rightFootHeel = document.getElementById('rightFootHeel');
+  manLeftFoot = document.getElementById('manLeftFoot')
+  manLeftFootToes = document.getElementById('manLeftFootToes')
+  manLeftFootHeel = document.getElementById('manLeftFootHeel')
+  manRightFoot = document.getElementById('manRightFoot')
+  manRightFootToes = document.getElementById('manRightFootToes')
+  manRightFootHeel = document.getElementById('manRightFootHeel')
+  womanLeftFoot = document.getElementById('womanLeftFoot')
+  womanLeftFootToes = document.getElementById('womanLeftFootToes')
+  womanLeftFootHeel = document.getElementById('womanLeftFootHeel')
+  womanRightFoot = document.getElementById('womanRightFoot')
+  womanRightFootToes = document.getElementById('womanRightFootToes')
+  womanRightFootHeel = document.getElementById('womanRightFootHeel')
 
-  updateFeet(steps[stepCounter.value]);
+  updateFeet(steps[stepCounter.value])
 
-  window.addEventListener('resize', () => updateFeet(steps[stepCounter.value]));
-});
+  window.addEventListener('resize', () => updateFeet(steps[stepCounter.value]))
+})
 
 const updateFeet = (step) => {
-  let screenWidth = document.getElementById('morphDiv').offsetWidth;
-  let screenHeight = document.getElementById('morphDiv').offsetHeight;
+  let screenWidth = document.getElementById('morphDiv').offsetWidth
+  let screenHeight = document.getElementById('morphDiv').offsetHeight
 
-  leftFoot.style.top = ((screenHeight * step.leftHeight - footHeightDifferenz) + 'px');
-  leftFoot.style.left = ((screenWidth * step.leftWidth - footWidthDifferenz) + 'px');
-  leftFoot.style.transform = `rotate(${step.leftRotate}deg)`;
-  leftFootToes.style.backgroundColor = step.leftFootToesActive ? 'blue' : 'lightblue';
-  leftFootHeel.style.backgroundColor = step.leftFootHeelActive ? 'blue' : 'lightblue';
+  manLeftFoot.style.top = screenHeight * step.man.leftFoot.height - footHeightDifferenz + 'px'
+  manLeftFoot.style.left = screenWidth * step.man.leftFoot.width - footWidthDifferenz + 'px'
+  manLeftFoot.style.transform = `rotate(${step.man.leftFoot.rotate}deg)`
+  manLeftFootToes.style.backgroundColor = step.man.leftFoot.footToesActive ? 'blue' : 'lightblue'
+  manLeftFootHeel.style.backgroundColor = step.man.leftFoot.footHeelActive ? 'blue' : 'lightblue'
+  manRightFoot.style.top = screenHeight * step.man.rightFoot.height - footHeightDifferenz + 'px'
+  manRightFoot.style.left = screenWidth * step.man.rightFoot.width - footWidthDifferenz + 'px'
+  manRightFoot.style.transform = `rotate(${step.man.rightFoot.rotate}deg)`
+  manRightFootToes.style.backgroundColor = step.man.rightFoot.footToesActive ? 'blue' : 'lightblue'
+  manRightFootHeel.style.backgroundColor = step.man.rightFoot.footHeelActive ? 'blue' : 'lightblue'
 
-  rightFoot.style.top = ((screenHeight * step.rightHeight - footHeightDifferenz) + 'px');
-  rightFoot.style.left = ((screenWidth * step.rightWidth - footWidthDifferenz) + 'px');
-  rightFoot.style.transform = `rotate(${step.rightRotate}deg)`;
-  rightFootToes.style.backgroundColor = step.rightFootToesActive ? 'blue' : 'lightblue';
-  rightFootHeel.style.backgroundColor = step.rightFootHeelActive ? 'blue' : 'lightblue';
-};
+  womanLeftFoot.style.top = screenHeight * step.woman.leftFoot.height - footHeightDifferenz + 'px'
+  womanLeftFoot.style.left = screenWidth * step.woman.leftFoot.width - footWidthDifferenz + 'px'
+  womanLeftFoot.style.transform = `rotate(${step.woman.leftFoot.rotate}deg)`
+  womanLeftFootToes.style.backgroundColor = step.woman.leftFoot.footToesActive ? 'red' : '#FFCCCB'
+  womanLeftFootHeel.style.backgroundColor = step.woman.leftFoot.footHeelActive ? 'red' : '#FFCCCB'
+  womanRightFoot.style.top = screenHeight * step.woman.rightFoot.height - footHeightDifferenz + 'px'
+  womanRightFoot.style.left = screenWidth * step.woman.rightFoot.width - footWidthDifferenz + 'px'
+  womanRightFoot.style.transform = `rotate(${step.woman.rightFoot.rotate}deg)`
+  womanRightFootToes.style.backgroundColor = step.woman.rightFoot.footToesActive ? 'red' : '#FFCCCB'
+  womanRightFootHeel.style.backgroundColor = step.woman.rightFoot.footHeelActive ? 'red' : '#FFCCCB'
+}
 
 const BackBtn = () => {
   if (stepCounter.value > 0) {
-    stepCounter.value--;
-    updateFeet(steps[stepCounter.value]);
+    stepCounter.value--
+    updateFeet(steps[stepCounter.value])
   }
-};
+}
 
 const NextBtn = () => {
   if (stepCounter.value < steps.length - 1) {
-    stepCounter.value++;
-    updateFeet(steps[stepCounter.value]);
+    stepCounter.value++
+    updateFeet(steps[stepCounter.value])
   }
-};
+}
 
-const NextBtnDisabled = computed(() => stepCounter.value >= steps.length - 1);
-const BackBtnDisabled = computed(() => stepCounter.value <= 0);
+const NextBtnDisabled = computed(() => stepCounter.value >= steps.length - 1)
+const BackBtnDisabled = computed(() => stepCounter.value <= 0)
 </script>
 
-<style scoped lang='scss'>
+<style scoped lang="scss">
 #morphDiv {
   width: 100%;
   height: 90vh;
   background-color: #eee;
   display: flex;
 
-  #leftFoot, #rightFoot {
+  #manLeftFoot,
+  #manRightFoot,
+  #womanLeftFoot,
+  #womanRightFoot{
     position: absolute;
-    transition: top 2s ease, left 2s ease, transform 2s ease; /* Transition für rotation hinzufügen */
+    transition:
+      top 2s ease,
+      left 2s ease,
+      transform 2s ease;
   }
 
   .innerFootSpacer {
     height: 10px;
   }
 
-  .toe, .heel {
+  .womanToe,
+  .womanHeel {
+    background-color: red;
+    width: 50px;
+  }
+
+  .menToe,
+  .womanHeel {
     background-color: blue;
     width: 50px;
   }
 
-  .toe {
+  .womanToe, .menToe {
     border-radius: 2rem 2rem 0 0;
     height: 60px;
   }
 
-  .heel {
+  .womanHeel, .menHeel {
     border-radius: 0 0 1rem 1rem;
     height: 40px;
   }
@@ -132,7 +274,8 @@ const BackBtnDisabled = computed(() => stepCounter.value <= 0);
     width: 50px;
   }
 
-  #backButton, #nextButton {
+  #backButton,
+  #nextButton {
     width: 10rem;
     height: 2.5rem;
     background-color: lightblue;
