af7e9c5ba6bb5f35f3fc96f9b768fabd4790adf8
Erik Weidenauer <erik.weidenauer@gmail.com>
2025-03-11 15:15:42 +0100

    Uploading basic setup

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import MainPageView from '../views/MainPageView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: MainPageView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
+})
+
+export default router
