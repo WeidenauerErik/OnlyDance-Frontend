ff54a3cd28af54fd55035469346e689bad049b90
Erik Weidenauer <erik.weidenauer@gmail.com>
2025-03-13 08:36:02 +0100

    finishing the prototype for the navbar
    not tests included

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -10,11 +10,11 @@ import {RouterLink} from "vue-router";
     </div>
 
     <nav id="linksContainerNavBar">
-      <input type="text" id="SearchBarNavBar">
-      <RouterLink to="/SignUpLogin" class="routerLinksNavBar">Login</RouterLink>
-      <RouterLink to="/SignUpLogin" class="routerLinksNavBar">SignUp</RouterLink>
+      <input type="text" id="searchBarNavBar">
+      <RouterLink to="/SignUpLogin" class="routerLinksNavBar" id="loginNavBar">Login</RouterLink>
+      <RouterLink to="/SignUpLogin" class="routerLinksNavBar" id="signUpNavBar">SignUp</RouterLink>
 
-      <p>Burger</p>
+      <img src="../assets/menuIcon.png" id="menuIconNavBar" alt="Menu button">
       <!--
       <RouterLink to="/" class="routerLinksNavBar">LandingPage</RouterLink>
       <RouterLink to="/Checklist" class="routerLinksNavBar">Checklist</RouterLink>
@@ -29,7 +29,7 @@ import {RouterLink} from "vue-router";
 #containerNavBar {
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
   background-color: $primary-color;
@@ -37,6 +37,7 @@ import {RouterLink} from "vue-router";
   #logoContainerNavBar {
     display: flex;
     flex-direction: row;
+    margin-left: 20px;
 
     #logoNavBar {
       width: 50px;
@@ -44,14 +45,44 @@ import {RouterLink} from "vue-router";
 
     #titleNavBar {
       font-size: 35px;
+      color: $basic-white;
     }
   }
 
   #linksContainerNavBar {
-    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-right: 20px;
+
+    #signUpNavBar {
+      border: $basic-white 1px solid;
+      padding: 10px;
+      border-radius: 10px;
+      margin: 0 20px 0 20px;
+    }
+
+    #loginNavBar {
+      margin-left: 20px;
+    }
+
+    #searchBarNavBar {
+      border: none;
+      border-radius: 20px;
+      height: 7vh;
+      font-size: 20px;
+    }
+
+    #searchBarNavBar:focus {
+      outline: none;
+    }
+
+    #menuIconNavBar {
+      width: 50px;
+    }
 
     .routerLinksNavBar {
-      color: $secondary-color;
+      color: $basic-white;
       text-decoration: none;
     }
 
