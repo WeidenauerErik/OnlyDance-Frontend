c27383fbb5157592e65d5c8582040288e02f2693
Theodosiusss <1202@htl.rennweg.at>
2025-03-13 13:24:23 +0100

    US16_Task46_InProgress_ImplementedBasicFooter

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,36 @@ import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
+
+  <div id="pageContainer">
+    <div id="contentWrap">
   <header>
     <NavBarComponent/>
   </header>
   <RouterView />
-  <footer>
+    </div>
+  <footer id="footer">
     <FooterComponent/>
   </footer>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+#pageContainer {
+  position: relative;
+  min-height: 100vh;
+
+
+  #contentWrap {
+    padding-bottom: 2.5rem; /* Footer height */
+  }
+
+  #footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 2.5rem; /* Footer height */
+  }
+}
+
 </style>
--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,20 +5,84 @@ import {RouterLink} from "vue-router";
 
 <template>
   <div id="footerContainer">
-    <p>OnlyDance</p>
-    <p>2025</p>
-    <p>Andreas Juchelka, Erik Weidenauer, Theodor Widmer</p>
-    <nav>
-      <RouterLink to="/">LandingPage</RouterLink>
-      <RouterLink to="/Checklist">Checklist</RouterLink>
-      <RouterLink to="/MainPage">MainPage</RouterLink>
-      <RouterLink to="/EventCalendar">EventCalendar</RouterLink>
-      <RouterLink to="/Imprint">Imprint</RouterLink>
-      <RouterLink to="/SignUpLogin">SignUpLogin</RouterLink>
+
+   <img src="../assets/logo.svg" id="logoNavBar" alt="Logo of OnlyDance">
+    <div id="visionContainer">
+      <span id="visionText">"Know what to dance!"</span>
+      - OnlyDance 2025
+    </div>
+
+    <nav id="linkContainer">
+      Quicklinks:
+      <ul>
+        <li>
+          <RouterLink to="/">LandingPage</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/Checklist">Checklist</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/MainPage">MainPage</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/EventCalendar">EventCalendar</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/Imprint">Imprint</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/SignUpLogin">SignUpLogin</RouterLink>
+        </li>
+      </ul>
     </nav>
-    <p>Contact: 1132@htl.rennweg.at</p>
+    <div id="contactInfoContainer">
+      <span>Contact: </span>
+      <span>Andreas Juchelka</span>
+      <span>1132@htl.rennweg.at</span>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
+
+#footerContainer {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: $tertiary-color;
+  color: white;
+
+  #linkContainer{
+    align-items: center;
+    justify-content: center;
+    font-size: 15px;
+  }
+
+
+  #logoNavBar {
+    width: 50px;
+  }
+
+  #visionContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+
+    #visionText {
+      font-size: 40px;
+    }
+  }
+
+  #contactInfoContainer {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+
+
 </style>
